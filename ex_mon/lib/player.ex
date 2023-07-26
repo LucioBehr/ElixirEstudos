e97d664a27899b11a0ff14001ffd2c533c0e7102
Lucio <lucio.behr@outlook.com>
2023-07-25 22:02:12 -0300

    commit 03 250723- projeto recuperado e estabilizado

--- ex_mon/lib/player.ex
+++ ex_mon/lib/player.ex
@@ -1,26 +1,15 @@
-defmodule ExMon.Player
-  do
-    @required_keys [:life, :name, :moves]
-    @max_life 100
+defmodule ExMon.Player do
+  @required_keys [:life, :name, :moves]
+  @max_life 100
 
+  @enforce_keys @required_keys
+  defstruct @required_keys
 
-
-    @enforce_keys @required_keys
-
-    defstruct  @required_keys
-
-    def build(name, move_rnd, move_avg, move_heal)
-      do
-        %ExMon.Player{
-          name: name,
-          life: @max_life,
-          moves: %{
-            move_avg: move_avg,
-            move_rnd: move_rnd,
-            move_heal: move_heal
-          }
-
-
-        }
-      end
+  def build(name, move_avg, move_rnd, move_heal) do
+    %ExMon.Player{
+      life: @max_life,
+      moves: %{move_avg: move_avg, move_rnd: move_rnd, move_heal: move_heal},
+      name: name
+    }
   end
+end
